a670c909dc10f800d3b8e533750fa2ea043f7027
ExasTools <300346373+ExasTools@users.noreply.github.com>
2026-07-24 12:00:53 -0600

    Retooled init function for actually useful debug commands

--- src/modules/debug.js
+++ src/modules/debug.js
@@ -9,8 +9,38 @@ export function debugStorageSave(key, json) {
 	console.groupEnd()
 }
 
-export function initDebugMsg() {
+export function initDebugTools() {
 	if (!debugging) return
 
-	console.log('[Quillworks] Debugging enabled.')
+		window.qwDebug = {
+			dump() {
+				for (const key of ['characters', 'places']) {
+					const json = localStorage.getItem(key)
+					const data = json ? JSON.parse(json) : []
+
+					console.group(`[Quillworks] ${key}`)
+					console.table(data)
+					console.log('Raw JSON:', json)
+					console.groupEnd()
+				}
+			},
+
+			raw(key) {
+				console.log(localStorage.getItem(key))
+			},
+
+			size(key) {
+				const json = localStorage.getItem(key) ?? ''
+				console.log(`${key}: ${new Blob([json]).size} bytes`)
+			},
+
+			clear(key) {
+				if (confirm(`Delete localStorage entry "${key}"?`)) {
+					localStorage.removeItem(key)
+					console.log(`Delete "${key}"`)
+				}
+			}
+		}
+
+	console.log(`[Quillworks] Debugging enabled. Try qwDebug.dump()`)
 }
